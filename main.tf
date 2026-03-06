49993f523817bf03ce07eb726853ec88b1224a3c
giridhar <sgiridhar91@gmail.com>
2026-03-06 12:19:33 +0530

    changed the public ip sku to standard

--- main.tf
+++ main.tf
@@ -22,7 +22,9 @@ resource "azurerm_public_ip" "pip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
+  sku                 = "Standard"
 }
+
 
 resource "azurerm_network_interface" "nic" {
   name                = "myNIC"
