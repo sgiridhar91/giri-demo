1b16520a0595d0ccf58d520735624267e720dfb2
giridhar <sgiridhar91@gmail.com>
2026-03-06 12:07:11 +0530

    fixed ssh public key

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = var.public_key
   }
 
   os_disk {
--- variables.tf
+++ variables.tf
@@ -13,3 +13,7 @@ variable "vm_name" {
 variable "admin_username" {
   default = "azureuser"
 }
+
+variable "public_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCmst/w99yRPkyQUxQu3lwvQ/va3tz6rYZHWVql7xr4jjMJm3ps+jfPf5GUgK+neNMBkVIkmSA1IdtlkplAXEsiBtInIFJ13J/r1cerVZZBYJUNWrczQqU5KMsR/B+ZjrkgBhy3VWwpPP/1WgKPyx+SVMbMQYOMx+E5HRMWA5pptgNcmb00iMqv6gOTt1ZMc84VEb6iNu/9SFzS8buh7zkNTq3Vcl2cOapGMu1jidMOaH8uOy7FantkfW6shbY2OrxfH29fDXSwF0RYFS5c6ycbvDqC6h5ADhmiI5d0aztgc3jazGQz2XH5Id3KM2HDgQxz5vS1XMT59kz0eBtuF/uU3iunlNOijtV2W/cks70d3mBJ1ZEccCPj1gB7kYIBiSQsZKw3xFFihIwvnremOinHNXv0k9dNygaX4IIAOW/YQtTmQk7OQBx5K1etQz1dKU1piSJphiYRFG8RfrNy/kpNmMdquyUlGsZpy9hAUON6275eOoY5W9VzF9C9RLmryFk= giridhar@ubuntu"
+}
